83f1d64c6fb550d74bde89ef3265d54e2ff81fce
kyki2025 <yunhairichu@gmail.com>
2025-07-29 12:28:30 +0800

    dd

--- dist/functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-// 中间件函数，处理所有请求
-export async function onRequest(context) {
-  // 添加通用响应头
-  const response = await context.next();
-  response.headers.set('X-Powered-By', 'TeaDiaryPro');
-  response.headers.set('X-Version', context.env.API_VERSION || '1.0.0');
-  
-  return response;
-}
